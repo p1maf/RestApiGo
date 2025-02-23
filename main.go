5f8f18c921d3c25dd93652ecf26147be2a1dd80b
Ilya <ilya-ermakov.2003@yandex.ru>
2025-02-23 15:16:13 +0300

    добавление эндпоинтов

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+var task string
+
+type TaskResponse struct {
+	Task string `json:"task"`
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello", task)
+}
+
+func SetTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var taskResponse TaskResponse
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&taskResponse); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	task = taskResponse.Task
+}
 
 func main() {
-	fmt.Println("RestApiGo")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/setTask", SetTaskHandler).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
